fded00167a0f31942f31613ed6326806d1300396
Cleiton <cleitonrmacedo@gmail.com>
2022-08-01 20:23:50 -0300

    fix: Requisito 5 - Validacao de token travava a aplicacao, faltava importar o dotenv

--- src/middlewares/validateToken.js
+++ src/middlewares/validateToken.js
@@ -1,22 +1,23 @@
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
 const validateToken = (req, _res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    const e = new Error('Token not found');
+    e.name = 'UnauthorizedError';
+    throw e;
+  }
   try {
-    const { authorization } = req.headers;
-    if (!authorization) {
-      const e = new Error('Token not found');
-      e.name = 'UnauthorizedError';
-      throw e;
-    }
     const { data } = jwt.verify(authorization, process.env.JWT_SECRET);
+    console.log(data, req.user);
     req.user = data;
-  } catch (_e) {
+    return next();
+  } catch (error) {
     const e = new Error('Expired or invalid token');
       e.name = 'UnauthorizedError';
       throw e;
   }
-
-  next();
 };
 
 module.exports = validateToken;
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const userController = require('../controllers/user.controller');
+const validateToken = require('../middlewares/validateToken');
 
 const router = express.Router();
 
+router.get('/', validateToken, userController.getAllUsers);
 router.post('/', userController.createUser);
-router.get('/', userController.getAllUsers);
 
 module.exports = router;
